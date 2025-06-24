0b771d81dc522a6bfb9025d48fe1bfb7549a3ffe
Certsoftt <ukpeubong8573@gmail.com>
2025-06-24 17:09:00 +0100

    fix space layout design for deliverables page

--- components/ui/FilterDropdown.tsx
+++ components/ui/FilterDropdown.tsx
@@ -11,7 +11,7 @@ const FilterDropdown: React.FC<FilterDropdownProps> = ({ label, options, value, 
   return (
     <div className="min-w-[120px]">
       <select
-        className="w-full px-4 py-2 rounded-md border border-grey-500 bg-white text-[#5a3ee6] font-medium focus:outline-primary-900 text-sm md:text-base shadow-sm"
+        className="w-full py-2 rounded-md border border-grey-500 bg-white text-[#5a3ee6] font-medium focus:outline-primary-900 text-sm md:text-sm shadow-sm"
         value={value}
         onChange={(e) => onChange(e.target.value)}
       >
