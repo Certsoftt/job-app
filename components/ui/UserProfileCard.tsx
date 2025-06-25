911d53d3455e3ff63b871b0929d56e7fb86cd097
Certsoftt <ukpeubong8573@gmail.com>
2025-06-26 00:40:57 +0100

    Update: UserProfileCard

--- components/ui/UserProfileCard.tsx
+++ components/ui/UserProfileCard.tsx
@@ -4,10 +4,10 @@ import { userProfile } from "@/utils/mockData";
 
 const UserProfileCard: React.FC = () => {
   return (
-    <div className="flex items-center gap-4 border border-[#553bd96d] px-4 py-3 rounded-xl">
+    <div className="flex items-center gap-2 border border-[#553bd96d] px-2 py-3 rounded-xl">
       <Image src={userProfile.avatar} alt="" width={40} height={40} />
       <div className="">
-        <h2 className="text-[#543CDA] font-semibold text-lg">
+        <h2 className="text-[#543CDA] font-semibold text-sm">
           {userProfile.name}
         </h2>
         <p className="font-light text-sm text-gray-500">
