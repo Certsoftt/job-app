ffa2630c4c09f0314b1b6172b222ac01f9a50451
Certsoftt <ukpeubong8573@gmail.com>
2025-06-25 20:57:23 +0100

    Update: rendering logic of empty state

--- components/ui/EmptyState.tsx
+++ components/ui/EmptyState.tsx
@@ -12,8 +12,8 @@ const EmptyState: React.FC<EmptyStateProps> = ({ icon, title, description,button
     <div className="flex flex-col items-center justify-center h-full py-24">
       <div className="mb-6 flex items-center justify-center relative min-h-[120px] min-w-[180px]">{icon}</div>
       <div className="font-extrabold text-2xl md:text-3xl text-[#232427] text-center mb-2 leading-tight">{title}</div>
-      {description && <div className="text-[#7a7a7a] text-center text-base md:text-lg mt-1">{description}</div>}
-      {button && <button className="cursor-pointer bg-[#5a3ee6] hover:bg-[#6d52e1] transition-colors text-white px-7 py-2.5 rounded-[8px] font-semibold text-[15px] shadow-none min-w-[150px] h-[40px] flex items-center justify-center" aria-label="Create new project">{button}</button>}
+      {description && <div className="text-[#7a7a7a] text-center text-base md:text-lg mt-1 mb-2">{description}</div>}
+      {button && <button className="mt-1 cursor-pointer bg-[#5a3ee6] hover:bg-[#6d52e1] transition-colors text-white px-7 py-2.5 rounded-[8px] font-semibold text-[15px] shadow-none min-w-[150px] h-[40px] flex items-center justify-center" aria-label="Create new project">{button}</button>}
     </div>
   );
 };
